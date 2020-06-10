cdf94aad6792a585b94bb48ab193eb4da4bd2564
V KRISHNA MOHAN <krishna1711mohan@yahoo.in>
2020-06-10 12:25:57 +0530

    Updated the folder structure of Project

---
+++ DataExtraction/ExtractionScripts/Demo.py
@@ -0,0 +1,6 @@
+print("Heloo this is a demo file")
+import os
+# curWorkDir = os.getcwd()
+# with open(curWorkDir+"//AllCityLinks//test.txt",'w+') as f:
+#     f.write("Test file creation hehe >>>>>")
+print(os.)
