2ecb5cebdb7e859d1ee40527ee92af4bcd987124
ajinkya96 <ajinkyabhide96@gmail.com>
2020-06-27 00:42:12 +0530

    Added IndianExpress.py under ExtractedScripts

--- DataExtraction/ExtractionScripts/Demo.py
+++
@@ -1,6 +0,0 @@
-print("Heloo this is a demo file")
-import os
-# curWorkDir = os.getcwd()
-# with open(curWorkDir+"//AllCityLinks//test.txt",'w+') as f:
-#     f.write("Test file creation hehe >>>>>")
-print(os.)
